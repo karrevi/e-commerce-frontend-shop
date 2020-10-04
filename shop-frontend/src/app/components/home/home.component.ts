56582d639cd5608d4069b24a9f924d13d790f151
Carlos Revilla <clro26.k@gmail.com>
2020-10-04 23:19:43 +0200

    exported model product and server response

--- shop-frontend/src/app/components/home/home.component.ts
+++ shop-frontend/src/app/components/home/home.component.ts
@@ -1,28 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ProductService } from 'src/app/services/product.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { ProductModelServer, ServerResponse } from "src/app/models/product.model";
+import { ProductService } from "src/app/services/product.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  products: ProductModelServer[] = [];
 
-  products: any[] = [];
-
-
-  constructor(private productService: ProductService,
-    private router: Router) { }
+  constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit(): void {
-    this.productService.getAllProducts().subscribe((prods: { count: Number, products: any[] }) => {
+    this.productService.getAllProducts().subscribe((prods: ServerResponse) => {
       this.products = prods.products;
-      console.log(this.products);
     });
   }
   selectProduct(id: Number) {
-    this.router.navigate(['/product', id]).then();
+    this.router.navigate(["/product", id]).then();
   }
-
 }
