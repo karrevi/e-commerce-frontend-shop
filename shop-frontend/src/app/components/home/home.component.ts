c4399df633270e7b2c9e2ffa8f87a8599df41fcc
Carlos Revilla <clro26.k@gmail.com>
2020-10-07 22:11:44 +0200

    declared fucntion add product to cart

--- shop-frontend/src/app/components/home/home.component.ts
+++ shop-frontend/src/app/components/home/home.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { ProductModelServer, ServerResponse } from "src/app/models/product.model";
+import {
+  ProductModelServer,
+  ServerResponse,
+} from "src/app/models/product.model";
+import { CatService } from "src/app/services/cat.service";
 import { ProductService } from "src/app/services/product.service";
 
 @Component({
@@ -9,10 +13,13 @@ import { ProductService } from "src/app/services/product.service";
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  
   products: ProductModelServer[] = [];
 
-  constructor(private productService: ProductService, private router: Router) {}
+  constructor(
+    private productService: ProductService,
+    private catService: CatService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.productService.getAllProducts().subscribe((prods: ServerResponse) => {
@@ -21,5 +28,8 @@ export class HomeComponent implements OnInit {
   }
   selectProduct(id: Number) {
     this.router.navigate(["/product", id]).then();
+  }
+  AddToCart(id: any) {
+    this.catService.AddProductToCart(id);
   }
 }
